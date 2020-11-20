e3661fbfafce9ba353c4bda5b472fbda6c532f3b
Adam Zoltan Nagy <adam.nagy@accolade.com>
2020-11-20 15:42:12 +0100

    add exception handling

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ def load_dictionary():
 
 def find_word(input_word):
     dictionary = load_dictionary()
-    print(f"{input_word.capitalize()}:")
-    for definition in dictionary[input_word]:
-        print(definition)
-
+    if input_word in dictionary:
+        print(f"{input_word.capitalize()}:")
+        for definition in dictionary.get(input_word):
+            print(definition)
+    else:
+        print("The word doesn't exist in the dictionary.")
 
 if __name__ == '__main__':
     word = input("Enter a word: ")
