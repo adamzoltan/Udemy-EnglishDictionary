f0a519d9a6ed31df9b0c7011c42593c647042ea8
Adam Zoltan Nagy <adam.nagy@accolade.com>
2020-11-20 18:23:50 +0100

    find similar word

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 
 def load_dictionary():
@@ -14,7 +15,12 @@ def find_word(input_word):
         for definition in dictionary.get(input_word):
             print(definition)
     else:
-        print("The word doesn't exist in the dictionary.")
+        close_match = get_close_matches(input_word, dictionary.keys(), n=1)
+        if close_match:
+            print(f"The word doesn't exist in the dictionary. Did you mean {close_match[0]}?")
+        else:
+            print("The word doesn't exist in the dictionary.")
+
 
 if __name__ == '__main__':
     word = input("Enter a word: ")
